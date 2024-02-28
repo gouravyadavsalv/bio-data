1bd1ef4144c3c3e8d7d59f6ebb9b94ab88cf5e5f
Devansh <devanshshrivastava07@gmail.com>
2024-02-28 14:26:43 +0530

    Initial Commit

---
+++ app/src/main/java/com/example/biodata2/MainActivity.kt
@@ -0,0 +1,33 @@
+package com.example.biodata2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val editTextName = findViewById<EditText>(R.id.editTextName)
+        val editTextPassword = findViewById<EditText>(R.id.editTextPassword)
+        val buttonSubmit = findViewById<Button>(R.id.buttonSubmit)
+
+        buttonSubmit.setOnClickListener {
+            val name = editTextName.text.toString()
+            val password = editTextPassword.text.toString()
+
+            if (name == "dev" && password == "1111") {
+                val intent = Intent(this, WelcomeActivity::class.java)
+                startActivity(intent)
+                finish()
+            } else {
+                Toast.makeText(this, "Invalid credentials", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/biodata2/WelcomeActivity.kt
@@ -0,0 +1,18 @@
+package com.example.biodata2
+
+
+import android.content.Context
+import android.content.Intent
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class WelcomeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+    }
+}
